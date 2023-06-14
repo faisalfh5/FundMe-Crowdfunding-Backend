4a6174dbb04fd66a64549ed07dd5418855dc038e
faisalfh5 <faisalh550@gmail.com>
2023-06-14 12:40:31 +0500

    Add User SignUp & Login

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+// Parent Routes
+router.use('/admins', require('./admins')); // All the user routes
+router.use('/users', require('./users')); // All the user routes
+router.use('/auth', require('./auth')); // All the auth routes
+
+// Export
+module.exports = router;
